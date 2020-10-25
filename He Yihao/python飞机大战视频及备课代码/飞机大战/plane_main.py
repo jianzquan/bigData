90517dd77eeeacc62244b7a2c0ddd9830da4347c
jianzquan <56023940+jianzquan@users.noreply.github.com>
2020-10-25 16:20:39 +0800

    Update plane_main.py

--- He Yihao/python飞机大战视频及备课代码/飞机大战/plane_main.py
+++ He Yihao/python飞机大战视频及备课代码/飞机大战/plane_main.py
@@ -1,42 +1,33 @@
 import pygame
 from plane_sprites import *
 
-
 class PlaneGame(object):
     """飞机大战主游戏"""
-
     def __init__(self):
         print("游戏初始化")
-
         # 1. 创建游戏的窗口
         self.screen = pygame.display.set_mode(SCREEN_RECT.size)
         # 2. 创建游戏的时钟
         self.clock = pygame.time.Clock()
         # 3. 调用私有方法，精灵和精灵组的创建
         self.__create_sprites()
-
         # 4. 设置定时器事件 - 创建敌机　1s
         pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
         pygame.time.set_timer(HERO_FIRE_EVENT, 500)
 
     def __create_sprites(self):
-
         # 创建背景精灵和精灵组
         bg1 = Background()
         bg2 = Background(True)
-
         self.back_group = pygame.sprite.Group(bg1, bg2)
-
         # 创建敌机的精灵组
         self.enemy_group = pygame.sprite.Group()
-
         # 创建英雄的精灵和精灵组
         self.hero = Hero()
         self.hero_group = pygame.sprite.Group(self.hero)
 
     def start_game(self):
         print("游戏开始...")
-
         while True:
             # 1. 设置刷新帧率
             self.clock.tick(FRAME_PER_SEC)
@@ -50,9 +41,7 @@ class PlaneGame(object):
             pygame.display.update()
 
     def __event_handler(self):
-
         for event in pygame.event.get():
-
             # 判断是否退出游戏
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
@@ -60,7 +49,6 @@ class PlaneGame(object):
                 # print("敌机出场...")
                 # 创建敌机精灵
                 enemy = Enemy()
-
                 # 将敌机精灵添加到敌机精灵组
                 self.enemy_group.add(enemy)
             elif event.type == HERO_FIRE_EVENT:
@@ -79,47 +67,35 @@ class PlaneGame(object):
             self.hero.speed = 0
 
     def __check_collide(self):
-
         # 1. 子弹摧毁敌机
         pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
-
         # 2. 敌机撞毁英雄
         enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
-
         # 判断列表时候有内容
         if len(enemies) > 0:
-
             # 让英雄牺牲
             self.hero.kill()
-
             # 结束游戏
             PlaneGame.__game_over()
 
     def __update_sprites(self):
-
         self.back_group.update()
         self.back_group.draw(self.screen)
-
         self.enemy_group.update()
         self.enemy_group.draw(self.screen)
-
         self.hero_group.update()
         self.hero_group.draw(self.screen)
-
         self.hero.bullets.update()
         self.hero.bullets.draw(self.screen)
 
     @staticmethod
     def __game_over():
         print("游戏结束")
-
         pygame.quit()
         exit()
 
 if __name__ == '__main__':
-
     # 创建游戏对象
     game = PlaneGame()
-
     # 启动游戏
     game.start_game()
